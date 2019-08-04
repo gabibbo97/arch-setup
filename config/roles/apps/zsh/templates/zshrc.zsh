0518f83c8370d383ff5d48b7bec0b655b727970f
Giacomo Longo <gabibbo97@gmail.com>
2019-08-04 23:09:57 +0000

    Add Zsh
    Signed-off-by: Giacomo Longo <gabibbo97@gmail.com>

---
+++ config/roles/apps/zsh/templates/zshrc.zsh
@@ -0,0 +1,28 @@
+# Plugins
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Completions
+autoload -Uz compinit
+compinit
+
+zstyle ':completion:*' menu select
+zstyle ':completion::complete:*' gain-privileges 1
+
+setopt COMPLETE_ALIASES
+
+# Prompt
+autoload -Uz promptinit
+promptinit
+prompt fade red
+
+# Aliases
+## Colors
+alias diff='diff --color=auto'
+alias grep='grep --color=auto'
+alias ls='ls --color=auto'
+## Functions
+alias cp='cp --reflink=auto'
+alias rm='rm --preserve-root'
+alias top='htop'
